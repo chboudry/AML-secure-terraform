59a3b017624e41644cd430ebc68b956ec771e143
chboudry <chboudry@outlook.com>
2022-11-08 16:59:47 +0100

    commenting aks part

--- README.md
+++ README.md
@@ -30,6 +30,7 @@
 Requirements:
 - Workspace v1_legacy_mode_enabled to false (this is by default in terraform)
 - egress_public_network_access="disabled" when you add the deployment to the managed online endpoint
+- Targetted env need to use private image only (=can't target mcr.microsoft.com)
 
 
 #### AKS with CNI
--- aks.tf
+++ aks.tf
@@ -6,36 +6,36 @@
 # az extension add --name k8s-extension
 # az k8s-extension create --name <extension-name> --extension-type Microsoft.AzureML.Kubernetes --config enableTraining=True enableInference=True inferenceRouterServiceType=LoadBalancer allowInsecureConnections=True inferenceLoadBalancerHA=False --cluster-type managedClusters --cluster-name <your-AKS-cluster-name> --resource-group <your-RG-name> --scope cluster
 
-resource "azurerm_kubernetes_cluster" "aks" {
-  name                = "aks-${var.name}-${var.environment}"
-  location            = azurerm_resource_group.default.location
-  resource_group_name = azurerm_resource_group.default.name
-  dns_prefix          = "${var.name}-k8s"
+# resource "azurerm_kubernetes_cluster" "aks" {
+#   name                = "aks-${var.name}-${var.environment}"
+#   location            = azurerm_resource_group.default.location
+#   resource_group_name = azurerm_resource_group.default.name
+#   dns_prefix          = "${var.name}-k8s"
 
-  default_node_pool {
-    name           = "system"
-    node_count     = 1
-    vm_size        = "Standard_DS2_v2"
-    vnet_subnet_id = azurerm_subnet.snet-aks.id
-  }
+#   default_node_pool {
+#     name           = "system"
+#     node_count     = 1
+#     vm_size        = "Standard_DS2_v2"
+#     vnet_subnet_id = azurerm_subnet.snet-aks.id
+#   }
 
-  network_profile {
-    network_plugin    = "azure"
-    load_balancer_sku = "standard"
-    outbound_type     = "userDefinedRouting"
-  }
+#   network_profile {
+#     network_plugin    = "azure"
+#     load_balancer_sku = "standard"
+#     outbound_type     = "userDefinedRouting"
+#   }
 
-  identity {
-    type = "SystemAssigned"
-  }
+#   identity {
+#     type = "SystemAssigned"
+#   }
 
-}
+# }
 
-resource "null_resource" "mlextension" {
-    provisioner "local-exec" {
-        command = "az k8s-extension create --name mlextension --extension-type Microsoft.AzureML.Kubernetes --config enableTraining=True enableInference=True inferenceRouterServiceType=LoadBalancer allowInsecureConnections=True inferenceLoadBalancerHA=False --cluster-type managedClusters --cluster-name ${azurerm_kubernetes_cluster.aks.name} --resource-group ${azurerm_resource_group.default.name} --scope cluster" 
-    }
-    depends_on = [
-      azurerm_kubernetes_cluster.aks
-    ]
-}
+# resource "null_resource" "mlextension" {
+#     provisioner "local-exec" {
+#         command = "az k8s-extension create --name mlextension --extension-type Microsoft.AzureML.Kubernetes --config enableTraining=True enableInference=True inferenceRouterServiceType=LoadBalancer allowInsecureConnections=True inferenceLoadBalancerHA=False --cluster-type managedClusters --cluster-name ${azurerm_kubernetes_cluster.aks.name} --resource-group ${azurerm_resource_group.default.name} --scope cluster" 
+#     }
+#     depends_on = [
+#       azurerm_kubernetes_cluster.aks
+#     ]
+# }
